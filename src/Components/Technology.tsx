6ab800651585573d4d0ff196db0b05b33ad1dba3
solly <solobarine@gmail.com>
2023-12-27 20:35:15 +0100

    Add Technology Reusable Component

---
+++ src/Components/Technology.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Technology = ({
+  name,
+  icon,
+  imagePath = null,
+  keyId,
+}: {
+  name: string
+  icon?: string
+  imagePath?: string | null
+  keyId: number
+}) => {
+  return (
+    <div
+      className='technology relative transition-all duration-500 ease-in'
+      key={keyId}
+    >
+      {icon ? <i className={` ${icon} text-4xl`}></i> : null}
+      {imagePath && !icon ? (
+        <img src={imagePath} alt={name} className='h-10' />
+      ) : null}
+      <p className='techName'>{name}</p>
+    </div>
+  )
+}
+
+export default Technology
