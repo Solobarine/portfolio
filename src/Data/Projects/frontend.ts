ddf4f2b04031bab2970a131b7551082d02b6833f
solly <solobarine@gmail.com>
2023-12-27 16:35:56 +0100

    Add Front End Project

---
+++ src/Data/Projects/frontend.ts
@@ -0,0 +1,47 @@
+const frontend = [
+  {
+    id: 0,
+    name: 'My NBA',
+    description:
+      'A Front End Application showing all NBA Teams and Selected information about each Team.',
+    features: [
+      'Responsive Design',
+      'Light and Dark Theme Support',
+      'All NBA Teams',
+      'Info about each Team',
+    ],
+    images: [
+      '/projects/nba/nba-1.png',
+      '/projects/nba/nba-2.png',
+      '/projects/nba/nba-3.png',
+    ],
+    languages: [
+      {
+        name: 'TypeScript',
+        icon: 'devicon-typescript-plain colored',
+      },
+      {
+        name: 'CSS Modules',
+        icon: 'devicon-css-plain colored',
+      },
+    ],
+    frameworksAndTools: [
+      {
+        name: 'NextJS',
+        icon: 'devicon-nextjs-original colored',
+      },
+    ],
+    links: [
+      {
+        name: 'See Demo',
+        link: 'https://my-nba-three.vercel.app',
+      },
+      {
+        name: 'See Code',
+        link: 'https://github.com/solobarine/my_nba',
+      },
+    ],
+  },
+]
+
+export default frontend
