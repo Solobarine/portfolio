cb697c5bba82c3942e5a372c29347bd063eb8a38
solly <solobarine@gmail.com>
2023-12-27 20:37:16 +0100

    Add About Me

---
+++ src/Pages/About.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='p-2 sm:py-12 bg-gray-100 mt-12' id='about'>
+      <h2 className='text-3xl font-bold text-center py-6'>About Me</h2>
+      <p className='text-justify sm:mx-24'>
+        As a proficient Full Stack Developer, I excel in crafting sophisticated
+        web applications through meticulous code and adherence to industry best
+        practices such as version control and testing. My commitment to seamless
+        teamwork guarantees optimal outcomes. I am dedicated to staying abreast
+        of full stack development advancements and embracing new challenges
+        consistently.
+      </p>
+    </div>
+  )
+}
+
+export default About
