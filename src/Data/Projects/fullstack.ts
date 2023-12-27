851d189f193101d5ca66ec6f30e79091c2520e44
solly <solobarine@gmail.com>
2023-12-27 16:35:40 +0100

    Add Full Stack Projects

---
+++ src/Data/Projects/fullstack.ts
@@ -0,0 +1,358 @@
+const fullstack = [
+  {
+    id: 0,
+    name: 'Vibely',
+    description: 'A Social Media Application for Posts and Messaging.',
+    features: [
+      'User Authentication',
+      'User Authorization',
+      'Multi-Factor Authentication',
+      'Email Verification',
+      'In-App Notifications',
+      'Asset Upload and Storage',
+      'Posts Creation',
+      'Likes and Commemnts',
+      'Friends and Friend Requests',
+      'Followers',
+    ],
+    images: [
+      '/projects/vibely/vibely-create.png',
+      '/projects/vibely/vibely-message.png',
+      '/projects/vibely/vibely-profile.png',
+    ],
+    languages: [
+      {
+        name: 'PHP',
+        icon: 'devicon-php-plain colored',
+      },
+      {
+        name: 'JavaScript',
+        icon: 'devicon-javascript-plain colored',
+      },
+      {
+        name: 'CSS',
+        icon: 'devicon-css-plain colored',
+      },
+    ],
+    frameworksAndTools: [
+      {
+        name: 'Laravel',
+        icon: 'devicon-laravel-plain colored',
+      },
+      {
+        name: 'VueJS',
+        icon: 'devicon-vuejs-plain colored',
+      },
+      {
+        name: 'JetStream',
+        image: '/technologies/jetstream.png',
+      },
+      {
+        name: 'Inertia',
+        image: '/technologies/inertia.png',
+      },
+      {
+        name: 'PostgreSQL',
+        icon: 'devicon-postgresql-plain colored',
+      },
+      {
+        name: 'AWS S3',
+        image: '/technologies/s3.png',
+      },
+      {
+        name: 'Mailtrap',
+        image: '/technologies/mailtrap.png',
+      },
+      {
+        name: 'TailwindCSS',
+        icon: 'devicon-tailwindcss-plain colored',
+      },
+    ],
+    links: [
+      {
+        name: 'See Demo',
+        link: '#',
+      },
+      {
+        name: 'See Code',
+        link: 'https://github.com/solobarine/vibely',
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: 'Gran Sonrisa',
+    description: 'A Car Dealership App for Purchasing Cars',
+    features: [
+      'User Authentication',
+      'Email Verification',
+      'Admin Overview',
+      'Payment Integration',
+      'Asset Upload and Storage',
+      'Car Listings',
+      'Favourites',
+    ],
+    images: [
+      '/projects/sonrisa/sonrisa-landing.png',
+      '/projects/sonrisa/gran_sonrisa1.png',
+      '/projects/sonrisa/gran_sonrisa2.png',
+      '/projects/sonrisa/sonrisa-car.png',
+    ],
+    languages: [
+      {
+        name: 'Ruby',
+        icon: 'devicon-ruby-plain colored',
+      },
+      {
+        name: 'JavaScript',
+        icon: 'devicon-javascript-plain colored',
+      },
+      {
+        name: 'CSS3',
+        icon: 'devicon-css3-plain colored',
+      },
+      {
+        name: 'HTML5',
+        icon: 'devicon-html5-plain colored',
+      },
+    ],
+    frameworksAndTools: [
+      {
+        name: 'Rails',
+        icon: 'devicon-rails-plain colored',
+      },
+      {
+        name: 'PostgreSQL',
+        icon: 'devicon-postgresql-plain colored',
+      },
+      {
+        name: 'Paystack',
+        image: '/technologies/paystack.svg',
+      },
+      {
+        name: 'AWS S3',
+        image: '/technologies/s3.png',
+      },
+      {
+        name: 'Mailtrap',
+        image: '/technologies/mailtrap.png',
+      },
+      {
+        name: 'SASS',
+        icon: 'devicon-sass-original colored',
+      },
+    ],
+    links: [
+      {
+        name: 'See Demo',
+        link: 'https://gran-sonrisa.onrender.com',
+      },
+      {
+        name: 'See Code',
+        link: 'https://github.com/Solobarine/gran_sonrisa',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Sollyverse',
+    description: 'A Travel Reservations Application',
+    features: [
+      'User Authentication',
+      'Admin Overview',
+      'Asset Upload and Storage',
+      'Destination Listings',
+      'Reservations',
+      'Likes',
+      'Reviews',
+    ],
+    images: [
+      '/projects/sollyverse/sollyverse.png',
+      '/projects/sollyverse/sollyverse-login.png',
+      '/projects/sollyverse/sollyverse-register.png',
+    ],
+    languages: [
+      {
+        name: 'JavaScript',
+        icon: 'devicon-javascript-plain colored',
+      },
+      {
+        name: 'CSS3',
+        icon: 'devicon-css3-plain colored',
+      },
+    ],
+    frameworksAndTools: [
+      {
+        name: 'React',
+        icon: 'devicon-react-original colored',
+      },
+      {
+        name: 'Express',
+        icon: 'devicon-express-original colored',
+      },
+      {
+        name: 'MongoDB',
+        icon: 'devicon-mongodb-plain colored',
+      },
+      {
+        name: 'Supabase',
+        image: '/technologies/supabase.png',
+      },
+    ],
+    links: [
+      {
+        name: 'See Demo',
+        link: 'https://sollyverse.vercel.app',
+      },
+      {
+        name: 'See Code',
+        link: 'https://github.com/Solobarine/sollyverse-travels',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Continental',
+    description: 'A Financial Application for Sending and Receiving Funds.',
+    features: [
+      'User Authentication',
+      'Deposit Funds',
+      'Send Funds',
+      'Transaction History',
+    ],
+    images: [
+      '/projects/continental/continental_landing.png',
+      '/projects/continental/continental.png',
+    ],
+    languages: [
+      {
+        name: 'JavaScript',
+        icon: 'devicon-javascript-plain colored',
+      },
+      {
+        name: 'CSS3',
+        icon: 'devicon-css3-plain colored',
+      },
+    ],
+    frameworksAndTools: [
+      {
+        name: 'Laravel',
+        icon: 'devicon-laravel-plain colored',
+      },
+      {
+        name: 'VueJS',
+        icon: 'devicon-vuejs-plain colored',
+      },
+      {
+        name: 'Pinia',
+        image: '/technologies/pinia.svg',
+      },
+      {
+        name: 'PostgreSQL',
+        icon: 'devicon-postgresql-plain colored',
+      },
+      {
+        name: 'JWT',
+        image: '/technologies/jwt.png',
+      },
+    ],
+    links: [
+      {
+        name: 'See Demo',
+        link: 'https://continental-kappa.vercel.app',
+      },
+      {
+        name: 'See Code',
+        link: 'https://github.com/Solobarine/continental',
+      },
+    ],
+  },
+  //  {
+  //    id: 4,
+  //    name: 'Go Buddy',
+  //    description: 'A Task Tracking Application for Self Management',
+  //    features: ['User Authentication', 'Tasks Creation', 'Update Task Status'],
+  //    languages: [
+  //      {
+  //        name: 'TypeScript',
+  //        icon: 'devicon-typescript-plain colored',
+  //      },
+  //      {
+  //        name: 'CSS3',
+  //        icon: 'devicon-css3-plain colored',
+  //      },
+  //    ],
+  //    frameworksAndTools: [
+  //      {
+  //        name: 'React',
+  //        icon: 'devicon-react-plain colored',
+  //      },
+  //      {
+  //        name: 'Redux',
+  //        icon: 'devicon-redux-plain colored',
+  //      },
+  //      {
+  //        name: 'Supabase',
+  //        image: '/technologies/supabase.png',
+  //      },
+  //    ],
+  //    links: [
+  //      {
+  //        name: 'See Demo',
+  //        link: '#',
+  //      },
+  //      {
+  //        name: 'See Code',
+  //        link: 'https://github.com/Solobarine/go_buddy',
+  //      },
+  //    ],
+  //  },
+  {
+    id: 5,
+    name: 'Nova',
+    description:
+      'An Application for Viewing Information About Favourite Series',
+    features: ['User Authentication', 'JWT', 'Use of TBMD', 'Categories'],
+    images: [
+      '/projects/nova/nova1.png',
+      '/projects/nova/nova-2.png',
+      '/projects/nova/nova3.png',
+    ],
+    languages: [
+      {
+        name: 'TypeScript',
+        icon: 'devicon-typescript-plain colored',
+      },
+      {
+        name: 'PHP',
+        icon: 'devicon-php-plain colored',
+      },
+    ],
+    frameworksAndTools: [
+      {
+        name: 'React',
+        icon: 'devicon-react-plain colored',
+      },
+      {
+        name: 'Laravel',
+        icon: 'devicon-laravel-plain colored',
+      },
+      {
+        name: 'PostgreSQL',
+        icon: 'devicon-postgresql-plain colored',
+      },
+    ],
+    links: [
+      {
+        name: 'See Demo',
+        link: 'https://nova-solly.fly.dev',
+      },
+      {
+        name: 'See Code',
+        link: 'https://github.com/Solobarine/nova',
+      },
+    ],
+  },
+]
+
+export default fullstack
