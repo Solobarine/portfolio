7da085e56389e3a7625a63eafa6f16b2a6605fff
solly <solobarine@gmail.com>
2023-12-27 20:28:33 +0100

    Create Nav Bar Component

---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <div className='sticky top-0 z-40 flex items-center justify-between p-2 bg-emerald-300'>
+      <div className='flex items-center gap-2'>
+        <img src='/logo.png' alt='My Logo' className='w-10' />
+        <span className='text-xl font-bold'>Solly</span>
+      </div>
+      <div
+        className={`flex flex-col fixed bg-gray-300 z-50 p-2 top-0 right-0 transition-all duration-500 ease-in h-screen bottom-0 w-56 lg:flex-row gap-4 ${
+          isOpen ? 'right-0' : 'right-[-100%]'
+        } lg:static lg:h-auto lg:w-auto lg:bg-transparent`}
+        onBlur={toggle}
+      >
+        <i
+          className={`fa-solid block transition-all duration-500 ease-in cursor-pointer ${
+            isOpen ? 'fa-square-xmark' : 'fa-bars fixed right-2'
+          } ml-auto text-2xl mt-1 lg:fixed lg:right-[-100%] lg:hidden`}
+          onClick={toggle}
+          onBlur={toggle}
+        ></i>
+        <a
+          href='#hero'
+          className={`p-2 flex items-center lg:mt-0 gap-2 text-md ${
+            !isOpen ? 'mt-10' : ''
+          }`}
+          onClick={toggle}
+        >
+          <i className='fa-solid fa-house'></i>
+          Home
+        </a>
+        <a
+          href='#skills'
+          className='p-2 flex items-center gap-2 text-md'
+          onClick={toggle}
+        >
+          <i className='fa-solid fa-screwdriver-wrench'></i> Skills
+        </a>
+
+        <a
+          href='#education'
+          className='p-2 flex items-center gap-2 text-md'
+          onClick={toggle}
+        >
+          <i className='fa-solid fa-building-columns'></i> Education
+        </a>
+
+        <a
+          href='#experience'
+          className='p-2 flex items-center gap-2 text-md'
+          onClick={toggle}
+        >
+          <i className='fa-solid fa-building'></i> Experience
+        </a>
+        <a
+          href='#projects'
+          className='p-2 flex items-center gap-2 text-md'
+          onClick={toggle}
+        >
+          <i className='fa-solid fa-briefcase'></i>
+          Projects
+        </a>
+        <a
+          href='#about'
+          className='p-2 flex items-center gap-2 text-md'
+          onClick={toggle}
+        >
+          <i className='fa-solid fa-user'></i>
+          About Me
+        </a>
+        <a
+          href='#contact'
+          className='p-2 flex items-center gap-2 text-md'
+          onClick={toggle}
+        >
+          <i className='fa-solid fa-address-card'></i>
+          Contact Me
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
