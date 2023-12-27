e6089459bafaee02cee5cefb45ecaff1ef9da55f
solly <solobarine@gmail.com>
2023-12-27 20:29:37 +0100

    Add Education Component

---
+++ src/Pages/Education.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import School from '../Components/School'
+import education from '../Data/education'
+
+const Education = () => {
+  return (
+    <div className='pt-12' id='education'>
+      <h2 className='text-3xl font-bold text-center py-6'>Education</h2>
+      <div className='education flex items-start justify-center flex-wrap gap-4'>
+        {education.map(data => (
+          <School key={data.id} data={data} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Education
