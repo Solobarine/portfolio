b599e49284b71c0cc598b7d9d1156bd0b0b656cb
solly <solobarine@gmail.com>
2024-01-03 11:35:02 +0100

    Add Variants

---
+++ src/Utils/variants.ts
@@ -0,0 +1,31 @@
+export const slidingVariant = () => {
+  return {
+    initial: { scale: 0, opacity: 0 },
+    visible: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: 'bounce',
+        delay: 0.7,
+      },
+    },
+  }
+}
+
+export const staggerVariant = (index: number) => {
+  return {
+    initial: {
+      translateX: '-20px',
+      opacity: 1,
+    },
+    visible: {
+      translateX: 0,
+      opacity: 1,
+      transition: {
+        delay: 1 + 1 * index,
+      },
+    },
+  }
+}
+
+export const bounceVariant = {}
