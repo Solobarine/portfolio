2a649874fecd0a65af36498e06812d67604503c2
solly <solobarine@gmail.com>
2024-01-29 14:31:50 +0100

    Update Hero Layout

--- src/Pages/Hero.tsx
+++ src/Pages/Hero.tsx
@@ -3,41 +3,44 @@ import Socials from '../Components/Socials'
 const Hero = () => {
   return (
     <div
-      className='relative sm:pl-36 py-14 sm:py-20 bg-gradient-to-r h-screen max-h-screen sm:max-h-[35em] from-emerald-700 to-slate-600 text-gray-100 grid place-content-center gap-3 p-2'
+      className='relative py-10 px-2 sm:pl-32 min-h-[70dvh] sm:min-h-screen'
       id='hero'
     >
-      <p>Hi, My Name is</p>
-      <h1 className='text-2xl sm:text-5xl font-bold py-2 text-gray-400'>
-        Solomon <span className='text-sky-500'>Akpuru</span>
-      </h1>
-      <h1 className='text-2xl sm:text-5xl font-bold sm:py-4 text-stone-400'>
-        I build Full Stack Applications
-      </h1>
-      <small className='font-bold sm:block'>
-        I am a Software Developer adept in Creating Memorable Digital
-        Experiences. <br /> Let's Build Something Today.
-      </small>
-      <Socials customStyles='py-6 text-gray-200 hidden sm:flex sm:flex-col sm:absolute sm:left-5 sm:top-1/2 sm:translate-y-[-50%]' />
-      <div className='mt-5'>
-        <a
-          href='/downloadables/solly_resume.pdf'
-          download
-          className='bg-sky-500 block w-fit my-5 px-4 py-3 rounded-lg'
-        >
-          Download My Resume
-        </a>
-        <div
-          className='contra-hire-me-button'
-          data-analyticsuserid='19a46a54-c52f-45bf-bf5e-82d33b8f7eb7'
-          data-theme='dark'
-          data-username='solomon_akpuru'
-        ></div>
+      <div className='z-10'>
+        <h1 className='text-2xl sm:text-3xl font-bold py-2 text-gray-400'>
+          Hello, I am <br />
+          Solomon <span className='text-sky-500'>Akpuru</span>
+        </h1>
+        <h1 className='text-2xl sm:text-3xl font-bold sm:py-4 text-stone-400'>
+          I build Full Stack Applications
+        </h1>
+        <small className='font-bold sm:block'>
+          I am a Software Developer adept in Creating Memorable Digital
+          Experiences. <br /> Let's Build Something Today.
+        </small>
+        <div className='mt-5 flex items-center gap-3'>
+          <a
+            href='/downloadables/solly_resume.pdf'
+            download
+            className='bg-sky-500 block w-fit my-5 px-4 py-3 rounded-lg'
+          >
+            Download My Resume
+          </a>
+          <div
+            className='contra-hire-me-button'
+            data-analyticsuserid='19a46a54-c52f-45bf-bf5e-82d33b8f7eb7'
+            data-theme='dark'
+            data-username='solomon_akpuru'
+          ></div>
+        </div>
       </div>
+      <Socials customStyles='z-20 sm:absolute sm:left-7 sm:top-0 sm:flex sm:flex-col sm:min-h-screen sm:justify-center gap-2' />
       <img
-        src='/laptop-1.png'
+        src='/logo.png'
         alt=''
-        className='block w-28 mt-12 absolute bottom-6 w-3/12 right-0'
+        className='block absolute right-0 -z-10 top-[0%] w-full max-w-[40em] right-0'
       />
+      <div className='bg-gray-200 absolute -z-20 inset-0'></div>
     </div>
   )
 }
