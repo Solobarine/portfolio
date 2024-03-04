ac2dd0ef19dbbfce6fc378ad2eba87b2427d81b8
Solly <solobarine@gmail.com>
2024-03-04 19:10:27 +0100

    Add Project Preview Component.

---
+++ src/Components/ProjectPreview.tsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+
+interface ProjectProps {
+  id: number;
+  name: string;
+  description: string;
+  features: string[];
+  images: string[];
+  languages: { name: string; icon?: string; image?: string }[];
+  frameworksAndTools: { name: string; icon?: string; image?: string }[];
+  links: { name: string; link: string }[];
+}
+
+const ProjectPreview = ({ data }: { data: ProjectProps }) => {
+  return (
+    <div className="p-3 border-lg w-full max-w-[25em] shadow-2xl shadow-slate-500 rounded-md">
+      <img src={data.images[0]} alt="" className="w-full aspect-video" />
+      <h2 className="font-2xl font-semibold">{data.name}</h2>
+      <p className="mt-2 text-sm">{data.description}</p>
+      <div className="flex items-center gap-2 mt-2 flex-wrap">
+        {data.frameworksAndTools.map((framework, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-3 flex-wrap px-1 py-2 rounded-lg bg-emerald-200"
+          >
+            {framework.icon ? (
+              <i className={`${framework.icon} text-xl`}></i>
+            ) : (
+              <img src={framework.image} alt={framework.name} className="h-6" />
+            )}
+            <p className="text-sm font-semibold">{framework.name}</p>
+          </div>
+        ))}
+      </div>
+      <div className="flex items-center justify-center gap-6 mt-2 py-1">
+        {data.links.map((link, index) => (
+          <Link
+            key={index}
+            to={link.link}
+            target="_blank"
+            className="text-sm font-bold text-blue-500"
+          >
+            {link.name}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectPreview;
