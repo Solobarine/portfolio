11b990e8088a1e0a00361f8594c0058f190704ed
Solly <solobarine@gmail.com>
2025-05-19 01:32:29 +0100

    create hook to scroll to hash fragments

---
+++ src/hooks/scroll.ts
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const ScrollToHash = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.getElementById(location.hash.replace("#", ""));
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
+
+  return null;
+};
+
+export default ScrollToHash;
